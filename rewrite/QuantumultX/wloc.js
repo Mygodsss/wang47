8b1cfdc0b229186929db9f0a67f8a4a6bfa2b30a
wang47hub <waq547@gmail.com>
2026-09-27 00:11:09 +0800

    Directly write correct wloc.js script content

--- rewrite/QuantumultX/wloc.js
+++ rewrite/QuantumultX/wloc.js
@@ -1,1 +1,5 @@
-404: Not Found
+/********************************
+ * Apple 虚拟定位与位置信息修正脚本
+ ********************************/
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'] || '';
+$done({});
